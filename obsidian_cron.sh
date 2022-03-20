cb7bd8358c5263ea36097d96558c6bd27c12ea1c
thomasabishop <tactonbishop@gmail.com>
2022-03-20 13:39:14 +0000

    Add Obsidian auto commit chron job

---
+++ obsidian_cron.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# If file changes exist in Obsidian directory, automate commit
+
+OBSIDIAN_PATH="${HOME}/Learning/Computer Science"
+
+cd "$OBSIDIAN_PATH"
+git pull
+CHANGES_EXIST="$(git status --porcelain | wc -l)"
+
+if [ "$CHANGES_EXIST" -eq 0 ]; then
+    exit 0
+fi
+
+git pull
+git add .
+git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")"
+git push -q
