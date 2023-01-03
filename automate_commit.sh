2c76fb06ed9f3e849ae460bd8067758776a878d3
thomasabishop <tactonbishop@gmail.com>
2023-01-03 12:28:52 +0000

    Add clean image dir to automate_commit.sh

--- automate_commit.sh
+++ automate_commit.sh
@@ -2,11 +2,13 @@
 # If file changes exist in notetakeing directory, automate commit
 
 NOTES_PATH="${HOME}/repos/computer_science"
-SPACE_2_UNDRSCR="${HOME}/repos/bash_scripts/space_to_underscore_filename.sh"
+SPACE_TO_UNDERSCORE="${HOME}/repos/bash_scripts/space_to_underscore_filename.sh"
+CLEAN_IMAGE_DIRECTORY="${HOME}/repos/computer_science/_scripts/clean_image_directory.sh"
 
 # Remove spaces from file names
 cd "$NOTES_PATH"
-source ${SPACE_2_UNDRSCR}
+source ${SPACE_TO_UNDERSCORE}
+source ${CLEAN_IMAGE_DIRECTORY}
 git pull
 CHANGES_EXIST="$(git status --porcelain | wc -l)"
 
